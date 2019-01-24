807bcb721095a5e405a18718accaf76799e5dc17
Nguyen Duc Lam <nguyenduclam1711@gmail.com>
2019-01-24 15:01:28 +0700

    add rot13 folder

---
+++ 0003-Rot13-#5kyu/main.js
@@ -0,0 +1,16 @@
+const rot13 = (message) => {
+    return message.split("")
+                  .map(function (x) {
+                        let charCode = x.charCodeAt(0)
+                        if((charCode >= 65 && charCode <= 77) || (charCode >= 97 && charCode <= 109)) return String.fromCharCode(charCode + 13)
+                        else if((charCode >= 78 && charCode <= 90) || (charCode >= 110 && charCode <= 122)) return String.fromCharCode(charCode - 13)
+                        else return String.fromCharCode(charCode)
+                  })
+                  .join("")
+}
+
+const showResult = () => {
+    document.getElementById('showResult').innerHTML = rot13(document.getElementById('inputText').value)
+}
+
+export default rot13
