ac25745df10a895053937c5786f7edc0ebb889c8
Nguyen Duc Lam <nguyenduclam1711@gmail.com>
2019-01-24 16:40:29 +0700

    modified main.js and added index.html

--- 0004-Are-they-the-same-#6kyu/main.js
+++ 0004-Are-they-the-same-#6kyu/main.js
@@ -1,13 +1,29 @@
+let string1 = '',
+    string2 = ''
 
 const comp = (array1, array2) => {
-    if (array1 === null || array2 === null) return 'false'
-    if (array1.length === 0 && array2.length === 0) return 'true'
-    
+    if (array1 === null || array2 === null) return false
+    if (array1.length === 0 && array2.length === 0) return true
+  
     let sortedArray1 = array1.sort((a,b) => a - b),
         sortedArray2 = array2.sort((a,b) => a - b).map(x => Math.sqrt(x))
-        
-    return sortedArray1.join(" ") === sortedArray2.join(" ") ? 'true' : 'false'
+      
+     return sortedArray1.join(" ") === sortedArray2.join(" ") ? true : false
 }
 
+const inputToArray = (string) => {
+    return string.split(",")
+}
 
+const onChangeHandler1 = () => {
+    string1 = document.getElementById('inputText1').value
+}
+
+const onChangeHandler2 = () => {
+    string2 = document.getElementById('inputText2').value
+}
+
+const showResult = () => {
+    document.getElementById('showResult').innerHTML = comp(inputToArray(string1), inputToArray(string2))
+}
 
