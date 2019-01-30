5b941e0ace7c4b2e6b228a4c27b8468f71de7c8c
OmegaLULer <nguyenduclam1711@gmail.com>
2019-01-31 00:29:01 +0700

    Modified main.js in 0012 folder
    delete 1 useless file

--- 0012-Find-the-missing-letter-#6kyu/main.js
+++ 0012-Find-the-missing-letter-#6kyu/main.js
@@ -1,5 +1,4 @@
 function findMissingLetter(array) {
-    let result = ''
     for(i = 0; i < array.length-1; i++) {
       if(array[i+1].charCodeAt(0) - array[i].charCodeAt(0) == 2)
         return String.fromCharCode(array[i].charCodeAt(0) + 1)
