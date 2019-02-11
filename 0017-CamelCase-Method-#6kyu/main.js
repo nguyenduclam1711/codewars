050a13ed0f03609b14c4d80c13e2946a19da0aff
Huy Nguyen <quanghuy.nguyen@savvycomsoftware.com>
2019-02-11 11:58:19 +0700

    another approach for 0017

--- 0017-CamelCase-Method-#6kyu/main.js
+++ 0017-CamelCase-Method-#6kyu/main.js
@@ -1,7 +1,8 @@
 String.prototype.camelCase = function(){
-    return this.trim().length != 0 ? this.trim().replace(/\s+/g, ' ').split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join('') : ''
+    return this.split(/\s/)
+                .filter(word => word !== "")
+                .reduce((acc, word) => acc + word[0].toUpperCase() + word.slice(1), '')
 }
-
 const   str = '   ',
         str1 = ' omega    lul    '
         
